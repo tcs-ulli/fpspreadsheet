d37370ce4c49afbc66de477ebf06297c55a5169e
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2009-01-09 08:39:40 +0000

    fpspreasheet: Adds forgotten files

---
+++ laz_fpspreadsheet.pas
@@ -0,0 +1,21 @@
+{ This file was automatically created by Lazarus. do not edit!
+  This source is only used to compile and install the package.
+ }
+
+unit laz_fpspreadsheet; 
+
+interface
+
+uses
+  fpolestorage, fpsallformats, fpsopendocument, fpspreadsheet, xlsbiff2, 
+  xlsbiff5, xlsbiff8, xlsxooxml, fpsutils, LazarusPackageIntf;
+
+implementation
+
+procedure Register; 
+begin
+end; 
+
+initialization
+  RegisterPackage('laz_fpspreadsheet', @Register); 
+end.
