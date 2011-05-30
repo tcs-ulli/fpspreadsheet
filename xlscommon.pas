a7106760d8a4e503de02389e1e3d98879f1eda42
sekelsenmat <sekelsenmat@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2011-05-30 09:22:08 +0000

    FPSpreadsheet: Adds some code for formula handling

--- xlscommon.pas
+++ xlscommon.pas
@@ -19,7 +19,17 @@ const
   INT_EXCEL_TOKEN_TSUB    = $04;
   INT_EXCEL_TOKEN_TMUL    = $05;
   INT_EXCEL_TOKEN_TDIV    = $06;
-  INT_EXCEL_TOKEN_TPOWER  = $07;
+  INT_EXCEL_TOKEN_TPOWER  = $07; // Power Exponentiation
+  INT_EXCEL_TOKEN_TCONCAT = $08;
+  INT_EXCEL_TOKEN_TLT     = $09; // Less than
+  INT_EXCEL_TOKEN_TLE     = $0A; // Less than or equal
+  INT_EXCEL_TOKEN_TEQ     = $0B; // Equal
+  INT_EXCEL_TOKEN_TGE     = $0C; // Greater than or equal
+  INT_EXCEL_TOKEN_TGT     = $0D; // Greater than
+  INT_EXCEL_TOKEN_TNE     = $0E; // Not equal
+  INT_EXCEL_TOKEN_TISECT  = $0F; // Cell range intersection
+  INT_EXCEL_TOKEN_TLIST   = $10; // Cell range list
+  INT_EXCEL_TOKEN_TRANGE  = $11; // Cell range
 
   { Constant Operand Tokens }
   INT_EXCEL_TOKEN_TNUM    = $1F;
@@ -78,6 +88,7 @@ type
     function GetLastRowIndex(AWorksheet: TsWorksheet): Integer;
     procedure GetLastColCallback(ACell: PCell; AStream: TStream);
     function GetLastColIndex(AWorksheet: TsWorksheet): Word;
+    function FormulaElementKindToExcelTokenID(AElementKind: TFEKind): Byte;
   end;
 
 implementation
@@ -129,6 +140,29 @@ begin
   FLastCol := 0;
   IterateThroughCells(nil, AWorksheet.Cells, GetLastColCallback);
   Result := FLastCol;
+end;
+
+function TsSpreadBIFFWriter.FormulaElementKindToExcelTokenID(
+  AElementKind: TFEKind): Byte;
+begin
+  case AElementKind of
+    { Operand Tokens }
+    fekCell:  Result := INT_EXCEL_TOKEN_TREFR;
+    fekCellRange: Result := INT_EXCEL_TOKEN_TRANGE;
+    fekNum:   Result := INT_EXCEL_TOKEN_TNUM;
+    { Basic operations }
+    fekAdd:   Result := INT_EXCEL_TOKEN_TADD;
+    fekSub:   Result := INT_EXCEL_TOKEN_TSUB;
+    fekDiv:   Result := INT_EXCEL_TOKEN_TDIV;
+    fekMul:   Result := INT_EXCEL_TOKEN_TMUL;
+    { Build-in Functions}
+    fekABS:   Result := INT_EXCEL_SHEET_FUNC_ABS;
+    fekROUND: Result := INT_EXCEL_SHEET_FUNC_ROUND;
+    { Other operations }
+    fekOpSUM: Result := 0;
+  else
+    Result := 0;
+  end;
 end;
 
 end.
