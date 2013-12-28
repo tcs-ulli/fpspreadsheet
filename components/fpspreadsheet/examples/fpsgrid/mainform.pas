8892a5c84fffbdd84ca8a64caa553f2f5ed0f903
bigchimp <bigchimp@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2013-12-28 15:29:22 +0000

    + Expanded fpsgrid example to be able to load/save first sheet of spreadsheet files.

--- components/fpspreadsheet/examples/fpsgrid/mainform.pas
+++ components/fpspreadsheet/examples/fpsgrid/mainform.pas
@@ -6,7 +6,8 @@ interface
 
 uses
   Classes, SysUtils, FileUtil, LResources, Forms, Controls, Graphics, Dialogs,
-  StdCtrls, fpspreadsheetgrid, fpspreadsheet;
+  StdCtrls, Menus,
+  fpspreadsheetgrid, fpspreadsheet, fpsallformats;
 
 type
 
@@ -14,8 +15,18 @@ type
 
   TForm1 = class(TForm)
     buttonPopulateGrid: TButton;
+    MainMenu1: TMainMenu;
+    mnuFile: TMenuItem;
+    mnuOpen: TMenuItem;
+    mnuQuit: TMenuItem;
+    mnuSaveAs: TMenuItem;
+    OpenDialog1: TOpenDialog;
+    SaveDialog1: TSaveDialog;
     sWorksheetGrid1: TsWorksheetGrid;
     procedure buttonPopulateGridClick(Sender: TObject);
+    procedure mnuOpenClick(Sender: TObject);
+    procedure mnuQuitClick(Sender: TObject);
+    procedure mnuSaveAsClick(Sender: TObject);
   private
     { private declarations }
   public
@@ -30,6 +41,7 @@ implementation
 { TForm1 }
 
 procedure TForm1.buttonPopulateGridClick(Sender: TObject);
+// Populate grid with some demo data
 var
   lWorksheet: TsWorksheet;
 begin
@@ -41,6 +53,40 @@ begin
     lWorksheet.Free;
   end;
 end;
+
+procedure TForm1.mnuOpenClick(Sender: TObject);
+// Loads first worksheet from file into grid
+begin
+  if OpenDialog1.Execute then
+  begin
+    sWorksheetGrid1.LoadFromSpreadsheetFile(OpenDialog1.FileName);
+  end;
+end;
+
+procedure TForm1.mnuQuitClick(Sender: TObject);
+begin
+  Close;
+end;
+
+procedure TForm1.mnuSaveAsClick(Sender: TObject);
+// Saves sheet in grid to file, overwriting existing file
+var
+  lWorkBook: TsWorkbook;
+  lWorkSheet:TsWorksheet;
+begin
+  if SaveDialog1.Execute then
+  begin
+    lWorkBook := TsWorkBook.Create;
+    lWorkSheet := lWorkBook.AddWorksheet('Sheet1');
+    try
+      sWorksheetGrid1.SaveToWorksheet(lWorkSheet);
+      lWorkBook.WriteToFile(SaveDialog1.FileName,true);
+    finally
+      lWorkBook.Free;
+    end;
+  end;
+end;
+
 
 initialization
   {$I mainform.lrs}
