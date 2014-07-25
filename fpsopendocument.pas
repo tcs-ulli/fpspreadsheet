1484e998779c4d6808fa4340f32f91e8b917cb45
wp_xxyyzz <wp_xxyyzz@8e941d3f-bd1b-0410-a28a-d453659cc2b4>
2014-07-25 16:31:34 +0000

    fpspreadsheet: Split shared code for ooxml and ods readers off into new unit fpsxmlcommon.pas

--- fpsopendocument.pas
+++ fpsopendocument.pas
@@ -39,7 +39,7 @@ uses
   fpspreadsheet,
   laz2_xmlread, laz2_DOM,
   AVL_Tree, math, dateutils,
-  fpsutils, fpsNumFormatParser;
+  fpsutils, fpsNumFormatParser, fpsxmlcommon;
   
 type
   TDateMode=(
@@ -71,7 +71,7 @@ type
 
   { TsSpreadOpenDocReader }
 
-  TsSpreadOpenDocReader = class(TsCustomSpreadReader)
+  TsSpreadOpenDocReader = class(TsSpreadXMLReader)
   private
     FCellStyleList: TFPList;
     FColumnStyleList: TFPList;
@@ -94,10 +94,6 @@ type
     function FindColumnByCol(AColIndex: Integer): Integer;
     function FindColStyleByName(AStyleName: String): integer;
     function FindRowStyleByName(AStyleName: String): Integer;
-    // Gets value for the specified attribute. Returns empty string if attribute
-    // not found.
-    function GetAttrValue(ANode : TDOMNode; AAttrName : string) : string;
-    function GetNodeValue(ANode: TDOMNode): String;
     procedure ReadColumns(ATableNode: TDOMNode);
     procedure ReadColumnStyle(AStyleNode: TDOMNode);
     // Figures out the base year for times in this file (dates are unambiguous)
@@ -968,36 +964,6 @@ begin
   Result := -1;
 end;
 
-function TsSpreadOpenDocReader.GetAttrValue(ANode : TDOMNode; AAttrName : string) : string;
-var
-  i : integer;
-  Found : Boolean;
-begin
-  Found:=false;
-  i:=0;
-  Result:='';
-  while not Found and (i<ANode.Attributes.Length) do begin
-    if ANode.Attributes.Item[i].NodeName=AAttrName then begin
-      Found:=true;
-      Result:=ANode.Attributes.Item[i].NodeValue;
-    end;
-    inc(i);
-  end;
-end;
-
-{ Returns the text value of a node. Normally it would be sufficient to call
-  "ANode.NodeValue", but since the DOMParser needs to preserve white space
-  (for the spaces in date/time formats), we have to go more into detail. }
-function TsSpreadOpenDocReader.GetNodeValue(ANode: TDOMNode): String;
-var
-  child: TDOMNode;
-begin
-  Result := '';
-  child := ANode.FirstChild;
-  if Assigned(child) and (child.NodeName = '#text') then
-    Result := child.NodeValue;
-end;
-
 procedure TsSpreadOpenDocReader.ReadBlank(ARow, ACol: Word; ACellNode: TDOMNode);
 var
   styleName: String;
@@ -1188,50 +1154,6 @@ var
   BodyNode, SpreadSheetNode, TableNode: TDOMNode;
   StylesNode: TDOMNode;
   OfficeSettingsNode: TDOMNode;
-
-  { We have to use our own ReadXMLFile procedure (there is one in xmlread)
-    because we have to preserve spaces in element text for date/time separator.
-    As a side-effect we have to skip leading spaces by ourselves. }
-  procedure ReadXMLFile(out ADoc: TXMLDocument; AFileName: String);
-  var
-    parser: TDOMParser;
-    src: TXMLInputSource;
-    stream: TStream;
-//    fstream: TStream;
-  begin
-    {
-    if (boBufStream in Workbook.Options) then begin
-      fstream := TFileStream.Create(AFilename, fmOpenRead + fmShareDenyWrite);
-      stream := TMemorystream.Create;
-      stream.CopyFrom(fstream, fstream.Size);
-      stream.Position := 0;
-      fstream.free;
-    end
-    }
-
-    if (boBufStream in Workbook.Options) then
-      stream := TBufStream.Create(AFileName, fmOpenRead + fmShareDenyWrite)
-    else
-      stream := TFileStream.Create(AFileName, fmOpenRead + fmShareDenyWrite);
-
-    try
-      parser := TDOMParser.Create;
-      try
-        parser.Options.PreserveWhiteSpace := true;    // This preserves spaces!
-        src := TXMLInputSource.Create(stream);
-        try
-          parser.Parse(src, ADoc);
-        finally
-          src.Free;
-        end;
-      finally
-        parser.Free;
-      end;
-    finally
-      stream.Free;
-    end;
-  end;
-
 begin
   //unzip files into AFileName path
   FilePath := GetTempDir(false);
